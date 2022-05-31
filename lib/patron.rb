d2f526f37b981d1dd5fb43e8566bc56126116a58
Monique Rio <mrio@umich.edu>
2022-05-31 10:19:32 -0400

    added job_description and campus_code

--- lib/patron.rb
+++ lib/patron.rb
@@ -1,6 +1,7 @@
 class Patron
   def initialize(data)
     @data = data
+    @inst_roll_map = YAML.load_file("./config/umich_inst_roles_map.yaml")
   end
 
   def uniqname
@@ -25,5 +26,19 @@ class Patron
 
   def external_id
     "SIS"
+  end
+
+  def campus_code
+    # can someone have multiple campus codes? can there be faculty at Flint and student in Ann Arbor?
+    @data["umichinstroles"].filter_map { |x| @inst_roll_map[x]["campus"] unless @inst_roll_map[x].nil? }.first
+  end
+
+  def job_description
+    @data["umichhr"].map do |x|
+      h = x.split(":").map do |y|
+        y.gsub(/[{}]/, "").split("=")
+      end.to_h
+      "#{h["deptDescription"]} (#{h["deptId"]})"
+    end.first
   end
 end
