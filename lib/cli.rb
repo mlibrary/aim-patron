95c347761bb7b7ae297086de8a611ab2cd9f4f9b
Monique Rio <mrio@umich.edu>
2025-10-22 15:05:00 -0400

    add full

--- lib/cli.rb
+++ lib/cli.rb
@@ -6,6 +6,19 @@ class CLI < Thor
     true
   end
 
+  desc "full", "processes all of the users in mcommunity"
+  method_option :output_directory, type: :string, required: false, default: S.output_directory, desc: "Path to directory for output files"
+  method_option :base_name, type: :string, required: false, default: "patron_full_#{Date.today.strftime("%Y%m%d")}", desc: "Basename for files put in the output directory"
+  method_option :size, type: :numeric, required: false, desc: "The maximum number of results to request"
+  def full
+    new_options = {
+      size: options[:size],
+      base_name: options[:base_name],
+      output_directory: options[:output_directory]
+    }
+    ProcessLdap.new(**new_options).process
+  end
+
   desc "range", "processes users for a given date range"
   method_option :start_date, type: :string, required: true
   method_option :end_date, type: :string, required: false, desc: "The end of the date range. Inclusive. If not given, it defaults to whatever start_date is."
@@ -39,7 +52,8 @@ class CLI < Thor
 
   no_commands do
     def format_date(date)
-      DateTime.parse(date).strftime("%Y%m%d")
+      date = DateTime.parse(date) if date.is_a? String
+      date.strftime("%Y%m%d")
     end
   end
 end
