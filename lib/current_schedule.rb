51fdb30f2ef03941ac62579efcba2297d3faba79
Monique Rio <mrio@umich.edu>
2026-06-05 17:28:03 -0400

    expiry date logic and tests

--- lib/current_schedule.rb
+++ lib/current_schedule.rb
@@ -18,3 +18,98 @@ class CurrentSchedule
     @current_config["Term"].split(", ").include?(term)
   end
 end
+
+class NewCurrentSchedule
+  FRIDAY_OFFSET = [5, 4, 3, 2, 1, 0, 6]
+  SUNDAY_OFFSET = [0, 6, 5, 4, 3, 2, 1]
+  attr_reader :today
+
+  def initialize(today)
+    @today = today
+  end
+
+  def expiry_date
+    case update_date.month
+    when 4
+      august_expiry_date(update_date.year)
+    when 8
+      january_expiry_date(update_date.year + 1)
+    when 12
+      may_expiry_date(update_date.year + 1)
+    end
+  end
+
+  def update_date
+    december_last_year = december_update_date(today.year - 1)
+    april_this_year = april_update_date(today.year)
+    august_this_year = august_update_date(today.year)
+    december_this_year = december_update_date(today.year)
+
+    if today >= december_last_year && today < april_this_year
+      december_last_year
+    elsif today >= april_this_year && today < august_this_year
+      april_this_year
+    elsif today >= august_this_year && today < december_this_year
+      august_this_year
+    else
+      december_this_year
+    end
+  end
+
+  private
+
+  def expiry_for_update_date
+    case update_date.month
+    when 4
+      august_expiry_date(update_date.year)
+    when 8
+      january_expiry_date(update_date.year + 1)
+    when 12
+      may_expiry_date(update_date.year + 1)
+    end
+  end
+
+  # last sunday in August
+  def august_expiry_date(year)
+    first = Date.new(year, 8)
+    offset = SUNDAY_OFFSET[first.wday]
+    fourth_sunday = Date.new(year, 8, 22 + offset)
+    return fourth_sunday + 7 if fourth_sunday.day < 25
+    fourth_sunday
+  end
+
+  # first sunday in August
+  def january_expiry_date(year)
+    first = Date.new(year)
+    offset = SUNDAY_OFFSET[first.wday]
+    Date.new(year, 1, 1 + offset)
+  end
+
+  # first sunday in May
+  def may_expiry_date(year)
+    first = Date.new(year, 5)
+    offset = SUNDAY_OFFSET[first.wday]
+    Date.new(year, 5, 1 + offset)
+  end
+
+  # second friday in december
+  def december_update_date(year)
+    first = Date.new(year, 12)
+    offset = FRIDAY_OFFSET[first.wday]
+    Date.new(year, 12, 8 + offset)
+  end
+
+  # first friday in April
+  def april_update_date(year)
+    first = Date.new(year, 4)
+    offset = FRIDAY_OFFSET[first.wday]
+    Date.new(year, 4, 1 + offset)
+  end
+
+  # first friday in August
+  def august_update_date(year)
+    first = Date.new(year, 8)
+    offset = FRIDAY_OFFSET[first.wday]
+    Date.new(year, 8, 1 + offset)
+  end
+end
--- spec/current_schedule_spec.rb
+++ spec/current_schedule_spec.rb
@@ -62,3 +62,60 @@ describe CurrentSchedule do
     end
   end
 end
+
+describe NewCurrentSchedule do
+  def expect_expiry_date(given, expected)
+    today = Date.parse(given)
+    expect(described_class.new(today).expiry_date).to eq(Date.parse(expected))
+  end
+
+  def expect_update_date(given, expected)
+    today = Date.parse(given)
+    expect(described_class.new(today).update_date).to eq(Date.parse(expected))
+  end
+  context "#expiry_date" do
+    it "works for a basic example" do
+      expect_expiry_date("2025-11-26", "2026-01-04")
+    end
+    it "handles May expiry dates" do
+      expect_expiry_date("2025-12-11", "2026-01-04")
+      # December update dates should be First Sunday in May
+      expect_expiry_date("2025-12-12", "2026-05-03")
+      expect_expiry_date("2026-12-11", "2027-05-02")
+    end
+
+    it "handles August expiry dates" do
+      # April update dates should return last Sunday in August
+      expect_expiry_date("2025-04-04", "2025-08-31")
+      expect_expiry_date("2026-04-03", "2026-08-30")
+      expect_expiry_date("2030-04-07", "2030-08-25")
+    end
+
+    it "handles Jaunary expiry dates" do
+      # August update dates should return first Sunday in Jaunary of next year
+      expect_expiry_date("2025-08-01", "2026-01-04")
+      expect_expiry_date("2026-08-07", "2027-01-03")
+    end
+  end
+  context "#update_date" do
+    it "handles when today is in the same month" do
+      # April before the April update date
+      expect_update_date("2025-04-01", "2024-12-13")
+
+      # April after the April update date
+      expect_update_date("2025-04-10", "2025-04-04")
+
+      # April on the April update date
+      expect_update_date("2025-04-04", "2025-04-04")
+
+      # August after the august update date
+      expect_update_date("2025-08-10", "2025-08-01")
+
+      # December bfore the december update date
+      expect_update_date("2025-12-11", "2025-08-01")
+
+      # Deceumber on the update date
+      expect_update_date("2025-12-12", "2025-12-12")
+    end
+  end
+end
